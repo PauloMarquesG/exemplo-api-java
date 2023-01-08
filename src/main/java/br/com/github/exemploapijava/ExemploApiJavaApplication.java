68f5809fb906daa3cc0af5ea37715319e982dd8a
paulo.goncalves <paulomarquesg06@gmail.com>
2023-01-08 20:25:26 -0300

    Projeto spring base

---
+++ src/main/java/br/com/github/exemploapijava/ExemploApiJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.github.exemploapijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/github/exemploapijava/ExemploApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.github.exemploapijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
